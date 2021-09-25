733f11977701523692035272a5a35fc82f3ca3b5
farelrz14 <67718586+farelrz14@users.noreply.github.com>
2021-09-24 20:25:39 -0600

    proyecto grupal

---
+++ Win.Escuela/FormLogin.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Escuela
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string usuario;
+            string contrasena;
+            string usuario1;
+            string contrasena1;
+
+            usuario = textBox1.Text;
+            contrasena = textBox2.Text;
+            usuario1 = textBox1.Text;
+            contrasena1 = textBox2.Text;
+
+            if (usuario == "master" && contrasena == "123")
+            {
+                this.Close();
+            }
+            else
+            {
+                if (usuario1 == "estu" && contrasena1 == "456")
+                {
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Usuario  o contraseña Invalido");
+                }
+              
+            }
+ 
+        }
+    }
+}
---
+++ Win.Escuela/FormMenu.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Escuela
+{
+    public partial class FormMenu : Form
+    {
+        public FormMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void loginToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Login();
+        }
+
+        private void Login()
+        {
+            var formLogin = new FormLogin();
+            formLogin.ShowDialog();
+        }
+
+        private void estudianteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formEstudiante = new FormEstudiante();
+            formEstudiante.MdiParent = this;
+            formEstudiante.Show();
+        }
+
+        private void maestroToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formMaestro = new FormMaestro();
+            formMaestro.MdiParent = this;
+            formMaestro.Show();
+        }
+
+        private void matriculaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var formMatricula = new FormMatricula();
+            formMatricula.MdiParent = this;
+            formMatricula.Show();
+        }
+
+        private void FormMenu_Load(object sender, EventArgs e)
+       {
+           Login();
+       }
+    }
+}
